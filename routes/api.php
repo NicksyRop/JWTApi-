a592f844e9405417cf60250d0fb07d059e89c1f1
NicksyRop <nicksonkipkorir25@gmail.com>
2022-06-07 17:23:02 +0300

    Added login register ,refresh methods to AuthController

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', 'AuthController@login');
+    Route::post('/register', 'AuthController@register');
+    Route::post('/logout', 'AuthController@logout');
+    Route::post('/refresh', 'AuthController@refresh');
+    Route::get('/user-profile', 'AuthController@userProfile');
 });
