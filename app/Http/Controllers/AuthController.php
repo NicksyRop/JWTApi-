919c0cb467c3dc6dc1b75c8a3787ff73ea57ff0f
NicksyRop <nicksonkipkorir25@gmail.com>
2022-06-08 09:31:40 +0300

    paasword string

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -45,7 +45,7 @@ class AuthController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|between:2,100',
             'email' => 'required|string|email|max:100|unique:users',
-            'password' => 'required|confirmed|min:6',
+            'password' => 'required|string|confirmed|min:6',
         ]);
         if ($validator->fails()) {
             return response()->json($validator->errors()->toJson(), 400);
